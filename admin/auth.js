1f64cce4973029da4db9430749e8aacc8d01ca7c
Bruno <150842026+baksistm@users.noreply.github.com>
2026-02-03 15:19:19 -0300

    Create auth.js

---
+++ admin/auth.js
@@ -0,0 +1,29 @@
+async function login() {
+const user = document.getElementById('user').value;
+const pass = document.getElementById('pass').value;
+const data = await getUsers();
+
+
+const found = data.find(u => u['Usuário'] === user && u['Senha'] === pass);
+
+
+if (!found) {
+document.getElementById('alert').style.display = 'block';
+return;
+}
+
+
+sessionStorage.setItem('auth', 'true');
+sessionStorage.setItem('nome', found['Nome Social']);
+
+
+window.location.href = 'loading.html';
+}
+
+
+function protect() {
+if (sessionStorage.getItem('auth') !== 'true') {
+alert('⚠️ Você deve realizar o login');
+window.location.href = 'login.html';
+}
+}
